1174b6a7b479ac09e167b257910509226bfbaa25
Avi Kainth <askainth2012@gmail.com>
2026-05-21 21:31:19 +0100

    Added Hamburger for phones

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,27 @@
 const toggleBtn = document.getElementById("theme-toggle");
 
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.textContent = "◐";
+} else {
+  toggleBtn.textContent = "◑";
+}
+
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
   if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
     toggleBtn.textContent = "◐";
   } else {
+    localStorage.setItem("theme", "light");
     toggleBtn.textContent = "◑";
   }
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
